5b7e9f4a3a0122b1deb9bae1cac0a1dfdd4714f8
Owen Harvey <80526926+owenharvey7@users.noreply.github.com>
2023-03-02 10:11:30 -0500

    Add files via upload

---
+++ SnakeTheGame/src/GameFrame.java
@@ -0,0 +1,23 @@
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame{
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3868855034012153296L;
+
+	GameFrame(){
+			
+		this.add(new GamePanel());
+		this.setTitle("Snake");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+		
+	}
+
+}
